34001712fb1ac189f42ca04480680403670abb6a
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-02-28 22:29:24 +0530

    Initial File Upload

---
+++ src/config/config_sample.h
@@ -0,0 +1,9 @@
+
+// This is the configuration file for the robot
+// Fill all the required the parameters and
+// rename this file as 'config.h' before use
+
+#ifndef _ROBOT_CONFIG_H
+#define _ROBOT_CONFIG_H
+
+#endif
