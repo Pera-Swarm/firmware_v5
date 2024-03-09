eb32cb4154c3a146fffdc2807ade607137503f3e
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-03-10 00:17:02 +0530

    Library and basic test setup

--- src/src.cpp
+++ src/src.cpp
@@ -2,41 +2,99 @@
 #include <Arduino.h>
 #include "define.h"
 
+// https://github.com/hideakitai/MPU9250
+#include "MPU9250.h"
+MPU9250 mpu; // You can also use MPU9255 as is
+
 int ROBOT_ID;
 
 // This is to hide non-test related source code.
 // https://docs.platformio.org/en/latest/plus/unit-testing.html
 #ifndef UNIT_TEST
 
+void print_roll_pitch_yaw()
+{
+    Serial.printf("%8.2f %8.2f %8.2f\n", mpu.getYaw(), mpu.getPitch(), mpu.getRoll());
+}
+
+void print_calibration()
+{
+    Serial.println("< calibration parameters >");
+    Serial.println("accel bias [g]: ");
+    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.println();
+    Serial.println("gyro bias [deg/s]: ");
+    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.println();
+    Serial.println("mag bias [mG]: ");
+    Serial.print(mpu.getMagBiasX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasZ());
+    Serial.println();
+    Serial.println("mag scale []: ");
+    Serial.print(mpu.getMagScaleX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleZ());
+    Serial.println();
+}
+
 void setup()
 {
-    // put your setup code here, to run once:
-
-    // Enables Serial Communication with baudRate of 115200
     Serial.begin(115200);
-    Serial.println("PeraSwarm Robot v5");
+    Wire.begin();
+    delay(2000);
 
-    memory.begin(); // NOTE: This should be run as the first thing.
+    if (!mpu.setup(0x68))
+    { // change to your own address
+        while (1)
+        {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+}
 
-    // This commands should be run 'ONLY' at the first run to assign a ID for robot
-    // memory.setupRobotWithId(RobotId)
-    // memory.setupRobotWithId(0);
-    ROBOT_ID = memory.getRobotId();
+void calibration()
+{
+    // calibrate anytime you want to
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
 
-    pinMode(PIN_LED_INBUILT, OUTPUT);
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
 
-    // Scan available I2C devices
-    i2c_scan();
+    print_calibration();
+    mpu.verbose(false);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-
-    digitalWrite(PIN_LED_INBUILT, HIGH);
-    delay(1000);
-    digitalWrite(PIN_LED_INBUILT, LOW);
-    delay(1000);
+    if (mpu.update())
+    {
+        static uint32_t prev_ms = millis();
+        if (millis() > prev_ms + 25)
+        {
+            print_roll_pitch_yaw();
+            prev_ms = millis();
+        }
+    }
 }
 
 #endif
